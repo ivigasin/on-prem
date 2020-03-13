f347ba493ffa3c5238bb146a1019e909c205f528
Igor Vigasin <vigasin@gmail.com>
2020-03-13 16:23:19 +0200

    Update install-kubernetes.sh

--- scripts/install-kubernetes.sh
+++ scripts/install-kubernetes.sh
@@ -17,7 +17,7 @@ apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
-EO
+EOF
 
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
