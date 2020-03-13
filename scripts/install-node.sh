5306140742349bbcd2cf1e2936575a4d6c07fe72
Igor Vigasin <vigasin@gmail.com>
2020-03-13 16:36:29 +0200

    Create install-node.sh
    kubernetes node

---
+++ scripts/install-node.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing docker"
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+
+echo "Installing kubernetes"
+
+apt-get update && apt-get install -y apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
