e7f3b81e71de91c4bd8f738042dfa30853caa5a5
Igor Vigasin <vigasin@gmail.com>
2020-03-13 16:21:53 +0200

    Update install-kubernetes.sh

--- scripts/install-kubernetes.sh
+++ scripts/install-kubernetes.sh
@@ -4,11 +4,11 @@ echo "Installing docker"
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
 
-add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-
-apt-get update && apt-get install -y docker-ce-$(apt-cache madison docker-ce | grep 19.03.8 | head -1 | aws '{print $3}')
 
 echo "Installing kubernetes"
 
